e04cfd1426633f649346794166531bae750b97b3
sonamchoki <sonamc282@gmail.com>
2018-10-18 19:19:38 +0600

    first upload

---
+++ app/src/main/java/com/example/karma/splashscreen/MainActivity_SPLASHscreen.java
@@ -0,0 +1,24 @@
+package com.example.karma.splashscreen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_SPLASHscreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT=4000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main__splashscreen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home = new Intent(MainActivity_SPLASHscreen.this,Navigation_MainActivity.class);
+                startActivity(home);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
